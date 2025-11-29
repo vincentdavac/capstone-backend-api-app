e5791c69a2e879b759005ed328023dedd484ad05
akosimario <166306717+akosimario@users.noreply.github.com>
2025-11-29 22:43:24 +0800

    apply descending and first para ma-retrieve yung last value sa broadcast

--- app/Http/Controllers/alertNotif.php
+++ app/Http/Controllers/alertNotif.php
@@ -11,7 +11,8 @@ class alertNotif extends Controller{
         $alerts = DB::table('alerts')->join('recent_alerts', 'alerts.alert_id', '=', 'recent_alerts.id')
             ->join('buoys', 'recent_alerts.buoy_id', '=', 'buoys.id')->where('buoys.barangay_id', $user->barangay_id)
             ->select('alerts.*','recent_alerts.description', 'recent_alerts.alert_level','recent_alerts.sensor_type','recent_alerts.recorded_at as alert_time',)
-            ->latest()->get();
+            ->orderBy('recent_alerts.recorded_at', 'desc')
+            ->first();
          return response()->json(['Success' =>true, 'data'=>$alerts], 200);
     }
 }
