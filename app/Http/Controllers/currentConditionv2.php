d3b727c14a1b34762a98a4dff8c4c74e24dfc779
akosimario <166306717+akosimario@users.noreply.github.com>
2025-12-11 22:59:00 +0800

    update v2

--- app/Http/Controllers/currentConditionv2.php
+++ app/Http/Controllers/currentConditionv2.php
@@ -11,59 +11,74 @@ class currentConditionv2 extends Controller
         $kh = DB::table('wind_readings')->select('wind_speed_k_h')->latest('recorded_at')->first();
         $windKh = (int)$kh->wind_speed_k_h;
         $status = null;
-        if($windKh >= 39 && $windKh <=61){
+        if ($windKh >= 39 && $windKh <= 61) {
             $status = "White";
-        }else if($windKh >=62 && $windKh <=88){
+        } else if ($windKh >= 62 && $windKh <= 88) {
             $status = "Blue";
-        }else if($windKh >= 89 && $windKh<=117){
+        } else if ($windKh >= 89 && $windKh <= 117) {
             $status = "Blue";
-        }
-        else{
+        } else {
             $status = "Red";
         }
-        return['wind_speed_k_h'=>$windKh,'staus'=>$status];
+        return ['wind_speed_k_h' => $windKh, 'staus' => $status];
     }
-     public function getwindMs(){
+    public function getwindMs(){
         $kh = DB::table('wind_readings')->select('wind_speed_m_s')->latest('recorded_at')->first();
         $windMs = (int)$kh->wind_speed_m_s;
         $status = null;
-        if($windMs >= 39 && $windMs <=61){
+        if ($windMs >= 39 && $windMs <= 61) {
             $status = "White";
-        }else if($windMs >=62 && $windMs <=88){
+        } else if ($windMs >= 62 && $windMs <= 88) {
             $status = "Blue";
-        }else if($windMs >= 89 && $windMs<=117){
+        } else if ($windMs >= 89 && $windMs <= 117) {
             $status = "Blue";
-        }
-        else{
+        } else {
             $status = "Red";
         }
         return ['wind_speed_m_s' => $windMs, 'status' => $status];
     }
     public function getdepth(){
-        $depth =DB::table('depth_readings')->select('depth_ft')->latest()->first();
-        $depthFeet =$depth->depth_ft;
+        $depth = DB::table('depth_readings')->select('depth_ft')->latest()->first();
+        $depthFeet = $depth->depth_ft;
         $div = $depthFeet / 17;
-        $finalData = $div *100;
+        $finalData = $div * 100;
         $status = null;
-        if($finalData <= 40){
+        if ($finalData <= 40) {
             $status = 'White';
-        }else if ($finalData >=41 && $finalData <=60){
-            $status ='Blue';
-        }else if($finalData >=61 && $finalData <=99){
+        } else if ($finalData >= 41 && $finalData <= 60) {
             $status = 'Blue';
-        }else{
-            $status ='Red';
+        } else if ($finalData >= 61 && $finalData <= 99) {
+            $status = 'Blue';
+        } else {
+            $status = 'Red';
         }
         return ['wind_speed_m_s' => $depthFeet, 'status' => $status];
     }
-     public function getCurrentCondition(){
+    public function getHumidity(){
+        $humidity = DB::table('bme280_humidity_readings')->select('humidity')->latest('recorded_at')->first();
+        $humidityData = $humidity->humidity;
+        $status = null;
+        if ($humidityData >= 30 && $humidityData <= 59) {
+            $status = "White";
+        }else if ($humidityData >= 60 && $humidityData <= 69) {
+            $status = "Blue";
+        }else if ($humidityData >= 25 && $humidityData <= 29) {
+            $status = "Blue";
+        }else if ($humidityData < 25) {
+            $status ="Red";
+        }else if ($humidityData > 70) {
+            $status = "Red";
+        }
+    }
+    public function getCurrentCondition()
+    {
         $data = DB::transaction(function () {
             return [
                 'windKh' => $this->getwindKh(),
-                'windms'=> $this->getwindMs(),
-                'waterDepth'=>$this->getdepth(),
+                'windms' => $this->getwindMs(),
+                'waterDepth' => $this->getdepth(),
             ];
         });
-       return response()->json(['success' => true, 'data' => $data], 200,[],JSON_PRETTY_PRINT);
+        return response()->json(['success' => true, 'data' => $data], 200, [], JSON_PRETTY_PRINT);
     }
 }
