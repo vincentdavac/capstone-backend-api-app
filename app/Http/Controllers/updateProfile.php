e89594ec97bf1e7ba4c031e0b4fc8ad810ed9e57
akosimario <166306717+akosimario@users.noreply.github.com>
2025-12-28 12:14:28 +0800

    add condition in updating brgy in profile

--- app/Http/Controllers/updateProfile.php
+++ app/Http/Controllers/updateProfile.php
@@ -16,6 +16,18 @@ class updateProfile extends Controller{
             'barangay' => 'nullable|string|max:255',
             'municipality' => 'nullable|string|max:255',
         ]);
+        $brgyId = null;
+        if($request->barangay == 'Barangay 160' || $request->barangay == 'Brgy 160' || $request->barangay == "160"){
+            $brgyId = 1;
+        }else if($request->barangay == 'Barangay 161' || $request->barangay == 'Brgy 161' || $request->barangay == '161'){
+            $brgyId = 2;
+        }else if($request->barangay == 'Barangay 162' || $request->barangay == 'Brgy 162' || $request->barangay == '162'){
+            $brgyId = 3;
+        }else if($request->barangay == 'Barangay 163' || $request->barangay == 'Brgy 163' || $request->barangay == '163'){
+            $brgyId = 4;
+        }else if($request->barangay == 'Barangay 164' || $request->barangay == 'Brgy 164' || $request->barangay == '164'){
+            $brgyId = 5;
+        }
         $user = $request->user();
         $user->first_name = $request->fname;
         $user->last_name =$request->lname;
@@ -23,7 +35,7 @@ class updateProfile extends Controller{
         $user->email = $request->email;
         $user->house_no =$request->house_no;
         $user->street = $request->street;
-        $user->barangay_id = $request->barangay;
+        $user->barangay_id = $brgyId;
         $user->municipality = $request->municipality;
         $user->save();
         return response()->json(['success' => true,'message' => 'updated successfully',], 200);
